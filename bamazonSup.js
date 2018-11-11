3798cdbdcbaede2c8f2fcdc345ba46cea37a4259
Kamran Sahrakar <41097862+ksahrakar@users.noreply.github.com>
2018-11-10 23:23:02 -0800

    more sup.js work

--- bamazonSup.js
+++ bamazonSup.js
@@ -14,6 +14,7 @@ var table = new Table({
     head: ['Dept ID','Dept Name','Overhead','Sales','Profit']
   , colWidths: [8,30,8,8,8]
 });
+dept=[];
 
 inq.prompt([
   {type:"list",
@@ -23,25 +24,47 @@ inq.prompt([
 ]).then(ans1 => {
   switch(ans1.choice) {
     case "Exit":console.log("Thanks for stopping by!");break;
-    case "View sales": viewSales();break;
+    case "View sales": 
+      getDepts();
+      for (i=0;i<dept.length;i++){
+        conn.query(`select item_sales from products where department_name = ${dept[i]}`, function(err,res){
+          if (err) throw err;
+          var sum = 0;
+          res.forEach(el=>{
+            sum+=el.item_sales;
+            console.log(sum);
+          });
+        conn.query(`update departments set sales = ${sum} where department_name = ${dept[i]}`, function(err2,res2){
+          if (err2) throw err2
+          else {conn.end()};
+      })
+    })
+  }
+      break;
     case "Add department": newDept();break;
   }
 });
 
 function viewSales(){
   console.log("You have chosen to view sales...");
-  var depts=[];
-  conn.query(`select distinct department_name from products`, function(err,res) {
+  
+};
+
+function getDepts(){
+  conn.query(`select distinct department_name from products`, function(err,res){
     if (err) throw err;
-    //console.log(res);
-    res.forEach(el=>{
-      console.log(el);
-      conn.query(`insert into departments (sales) values ${el.sum(item_sales)} from products where department_name = '${el.department_name}'`,function(err2,res2){
-        if (err2) throw err2;
-        console.log("Sales updated!");conn.end();
-      });
-    });
-  });  
+    res.forEach(el =>{
+      dept.push(el.department_name)
+    });console.log(dept);
+  });conn.end();
+};
+
+function getTotals(){
+  conn.query(`select item_sales from products where department_name='apparel'`, function(err,res){
+    if (err) throw err;
+    var sum=0;
+    res.forEach
+  })
 }
 
 function newDept(){
